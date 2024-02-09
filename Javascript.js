2089f173bb0cd79ea7e5a2e6df0c020e2deb13bb
Scott Thiffault <1854548@collegemv.qc.ca>
2024-02-09 11:37:27 -0500

    Ajout d'une fonctionnalité qui permet à l'utilisateur de visionner son nom à l'ouverture de la page d'acceuil

--- Javascript.js
+++ Javascript.js
@@ -1,12 +1,30 @@
-
+//Constante de la page d'acceuil de l'utilisateur
 const InputTache = document.getElementById("InputTache");
 const BtnAjoutTache = document.getElementById("BtnAjoutTache");
 const ListeDeTache = document.getElementById("ListeDeTache");
-
+//Constante de la page de LOG IN
+const Username = document.getElementById("Username");
+const Password = document.getElementById("Password");
 var task = "ListeTache";
 var i = 0;
 
 BtnAjoutTache.addEventListener("click", AjoutTache);
+
+function save(){
+    if (Username !== ""){
+    User = Username.value
+    localStorage.setItem("Username", User);
+    
+    localStorage.setItem("Password", Password);
+    window.location.href = "Accueil.html";
+    }
+    else{
+        alert("Veuillez entrer un nom d'utilisateur valide");
+    }
+}
+
+let Bienvenue = document.getElementById("Welcome")
+Bienvenue.innerHTML = "Bienvenue " + localStorage.getItem("Username") + " Chez Air Canada"
 
 function AjoutTache() {
     const TexteTache = InputTache.value.trim();
